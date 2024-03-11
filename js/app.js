ca7e79fe17614a6991aeadc8e6df58150924318c
Balduistra <ppereirab.jr@gmail.com>
2024-03-10 22:18:04 -0300

    refatorar codigo

--- js/app.js
+++ js/app.js
@@ -1,17 +1,18 @@
-// let seleção = document.getElementById('produto');
-// let fone = seleção.querySelector('select option[value="Fone de ouvido - R$100"]').value;
-// let celular = seleção.querySelector('select option[value="Celular - R$1400"]').value; 
-// let oculosVR = seleção.querySelector('select option[value="Oculus VR - R$5000"]').value;
-
-// let quantidade = document.getElementById('quantidade').value;
-
 function adicionar() {
-    let selecao = document.getElementById('produto');
-    let fone = parseInt(selecao.querySelector('select option[value="Fone de ouvido - R$100"]').value.split(' - ')[1].replace('R$', ''));
-    let celular = parseInt(selecao.querySelector('select option[value="Celular - R$1400"]').value.split(' - ')[1].replace('R$', ''));
-    let oculosVR = parseInt(selecao.querySelector('select option[value="Oculus VR - R$5000"]').value.split(' - ')[1].replace('R$', ''));
+//através do Id foi selecionado o elemento Select dentro do documento HTML
+    let produto = document.getElementById('produto');
+//alocando o value de cada seleção 
+    let itemSelecionado = produto.value;
+//fazendo a divisão do value em dois valores apartir da string " - " pelo metodo split oq
+// da uma array onde o indice [1] representa a segunda parte. 
+//com o metodo replace, substitui-se o primeiro valor "R$" por uma string vazia sobrando só o numero;
+//para transformar a string em um numero se usa o metodo parseInt.
+    let preco = parseInt(itemSelecionado.split(' - ')[1].replace('R$', ''));
+//pegando o valor do elemento 'quantidade' pelo id;
     let quantidade = parseInt(document.getElementById('quantidade').value);
-    let adicionado = quantidade * fone;
-    console.log('valor adicionado: ' + adicionado);
+
+    let valor  = preco * quantidade;
+    console.log('o valoe é = ' + valor);
+
 }
 
