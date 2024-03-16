6c0fb27e9e64b980f3b1ba8fcb9bd933c6f0703c
Balduistra <ppereirab.jr@gmail.com>
2024-03-16 07:41:40 -0300

    salvano

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
+
+let listaDeProdutos = []
 function adicionar() {
 //através do Id foi selecionado o elemento Select dentro do documento HTML
     let produto = document.getElementById('produto');
 //alocando o value de cada seleção 
     let itemSelecionado = produto.value;
+
+    
 //fazendo a divisão do value em dois valores apartir da string " - " pelo metodo split oq
 // da uma array onde o indice [1] representa a segunda parte. 
 //com o metodo replace, substitui-se o primeiro valor "R$" por uma string vazia sobrando só o numero;
@@ -10,9 +14,39 @@ function adicionar() {
     let preco = parseInt(itemSelecionado.split(' - ')[1].replace('R$', ''));
 //pegando o valor do elemento 'quantidade' pelo id;
     let quantidade = parseInt(document.getElementById('quantidade').value);
+    let nomeItem = itemSelecionado.split(' - ')[0];
+    let valor = preco * quantidade;
 
-    let valor  = preco * quantidade;
-    console.log('o valoe é = ' + valor);
+    let carrinho = document.querySelector('.carrinho__produtos__produto');
+    carrinho.innerHTML = carrinho.innerHTML + `<br><span class="texto-azul">${quantidade}</span> ${nomeItem} <span class="texto-azul">R$ ${valor}</span>`
 
+    
+    
+    
+    let carrinhoTotal = document.getElementById('valor-total');
+    let valorTotal = 1400;
+    carrinhoTotal.innerHTML = `R$${ + }`
+    return  valorTotal
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function nome (value) {
+//     let nome = produto.querySelector(`select option[value="${value}"]`)
+   
+// }
+
+
 
